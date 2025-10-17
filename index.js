17a5cdf5472311a72ba1c5d62b5342b7b668ae21
Numsik <honzik.klusek.myto@gmail.com>
2025-10-17 13:15:44 +0200

    first changes to binary tree

---
+++ index.js
@@ -0,0 +1,37 @@
+function sortedArrayToBSTRecur(arr, start, end) {
+    if (start > end) return null;
+
+    let mid = start + Math.floor((end - start) / 2);
+    let root = new Node(arr[mid]);
+
+    // Divide from middle element
+    root.left = sortedArrayToBSTRecur(arr, start, mid - 1);
+    root.right = sortedArrayToBSTRecur(arr, mid + 1, end);
+
+    return root;
+}
+
+// Function which return BST
+function sortedArrayToBST(arr) {
+    return sortedArrayToBSTRecur(arr, 0, arr.length - 1);
+}
+
+
+
+
+
+class node{
+    constructor(data = null,left = null, right = null){
+        this.data = data
+        this.left = left 
+        this.right = right
+    }
+}
+function sortedarray(array, start, end){
+    const mid = array.length / 2;
+    const firsthalf = array.slice(0,mid);
+    const secondhalf = array.slice(mid, -1)
+    return {firsthalf, secondhalf}
+}
+console.log(sortedarray([1,5,6,10,5,4]))
+
